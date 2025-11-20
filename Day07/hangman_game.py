557f99d1f92885503e37332aa04622724560a8a3
dilyanabodurova <diliana.bodurova@abv.bg>
2025-11-20 09:39:54 +0200

    hangman last version

--- Day07/hangman_game.py
+++ Day07/hangman_game.py
@@ -1,16 +1,8 @@
 import random
-import hangman_lost_lives
+import hangman_art
+from hangman_words import word_list
 
-word_list = ['aardvark', 'baboon', 'camel']
-print('''
- _                                             
-| |                                            
-| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
-| '_ \ / _' | '_ \ / _' | '_ ' _ \ / _' | '_ \ 
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
-|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    __/ |                      
-                   |___/''')
+print(hangman_art.hangman_logo)
 
 chosen_word = random.choice(word_list)
 letters_count = len(chosen_word)
@@ -43,29 +35,29 @@ while lives > 0 and not is_user_win:
                 temp_index +=1
             presented_word_str = "".join(presented_word)
             print(presented_word_str)
-            print(hangman_lost_lives.hangman)
+            print(hangman_art.hangman)
         else:
             lives = lives - 1
             guessed_letters.append(letter)
             print(f"You guessed {letter}, that's not in the word. You lose a life.")
             if lives == 0:
-                print(hangman_lost_lives.hangman_minus_six)
+                print(hangman_art.hangman_minus_six)
             elif lives ==1:
-                print(hangman_lost_lives.hangman_minus_five)
+                print(hangman_art.hangman_minus_five)
                 print_left_lives(lives)
             elif lives == 2:
-                print(hangman_lost_lives.hangman_minus_four)
+                print(hangman_art.hangman_minus_four)
                 print_left_lives(lives)
             elif lives == 3:
-                print(hangman_lost_lives.hangman_minus_three)
+                print(hangman_art.hangman_minus_three)
                 print_left_lives(lives)
             elif lives == 4:
-                print(hangman_lost_lives.hangman_minus_two)
+                print(hangman_art.hangman_minus_two)
                 print_left_lives(lives)
             elif lives == 5:
-                print(hangman_lost_lives.hangman_minus_one)
+                print(hangman_art.hangman_minus_one)
                 print_left_lives(lives)
     is_user_win =not any(x == "_" for x in presented_word)
 
 if is_user_win:
-    print("You win!")
+    print("****************************You win!****************************")
