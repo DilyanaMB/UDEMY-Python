fdee6031691dfc69b6fc533bfeffd27198dae59b
dilyanabodurova <diliana.bodurova@abv.bg>
2025-11-20 08:16:34 +0200

    a

--- Day07/hangman_game.py
+++ Day07/hangman_game.py
@@ -1,6 +1,6 @@
-from random import random
+import random
 
-word_list = ['aardvark','baboon','camel']
+word_list = ['aardvark', 'baboon', 'camel']
 print('''
  _                                             
 | |                                            
@@ -10,7 +10,25 @@ print('''
 |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
                     __/ |                      
                    |___/''')
-print("Word to guess: _______")
 chosen_word = random.choice(word_list)
-print(chosen_word)
-letter = input("Guess a letter: ")
+letters_count = len(chosen_word)
+guessed_letters = []
+presented_word = []
+
+for i in range(letters_count):
+    presented_word.append("_")
+presented_word_str = "".join(presented_word)
+print("Word to guess: " + presented_word_str)
+
+lives = 6
+is_user_win = False
+
+while lives > 0 or is_user_win:
+    letter = input("Guess a letter: ").lower()
+    if letter in guessed_letters:
+        continue
+    else:
+        if letter in chosen_word:
+            for i in chosen_word:
+                if i == letter:
+                    guessed_letters.append(letter[i])
