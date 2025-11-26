c3b5e5bc86013392d677ad81c9ffd3897f2a59f4
dilyanabodurova <diliana.bodurova@abv.bg>
2025-11-26 07:52:28 +0200

    refactor

--- Day012/guess_the_number.py
+++ Day012/guess_the_number.py
@@ -23,17 +23,19 @@ def check_if_number_guessed(u_guess, ch_number):
             print("Too low!")
     return is_guessed
 
-print("I'm thinking of a number between 1 and 100.")
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-chosen_number = random.randint(1,100)
-lives = set_difficulty(difficulty)
+def new_game():
+    print("I'm thinking of a number between 1 and 100.")
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    chosen_number = random.randint(1,100)
+    lives = set_difficulty(difficulty)
 
-is_number_guessed = False
-while lives > 0 and not is_number_guessed:
-    print(f"You have {lives} attempts remaining to guess the number.")
-    lives -= 1
-    user_guess = int(input("Make a guess: "))
-    is_number_guessed = check_if_number_guessed(user_guess, chosen_number)
+    is_number_guessed = False
+    while lives > 0 and not is_number_guessed:
+        print(f"You have {lives} attempts remaining to guess the number.")
+        lives -= 1
+        user_guess = int(input("Make a guess: "))
+        is_number_guessed = check_if_number_guessed(user_guess, chosen_number)
+    if not is_number_guessed:
+        print("You've run out of guesses. Refresh the page to run again.")
 
-if not is_number_guessed:
-    print("You've run out of guesses. Refresh the page to run again.")
+new_game()
