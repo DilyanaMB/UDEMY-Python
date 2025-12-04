b282c47a6ebd404a3fd976a90858e038c5754b29
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-04 21:53:07 +0200

    daniel hirst

--- Day018/challange5.py
+++ Day018/challange5.py
@@ -8,9 +8,8 @@ colours = ['chartreuse','DarkOrange','DeepPink','purple','yellow','SlateBlue1','
 timmy.speed('fastest')
 
 # circle with radius of 100
-for _ in range(40):
+for _ in range(80):
     timmy.color(choice(colours))
     timmy.circle(100)
-    timmy.forward(1)
-    timmy.right(10)
+    timmy.right(5)
 screen.exitonclick()
