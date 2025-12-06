a4944a7ae8437623eecec0d4a6337b31faae755e
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-06 13:50:39 +0200

    move in diff directions

--- Day020/snake.py
+++ Day020/snake.py
@@ -1,12 +1,18 @@
 from turtle import Turtle
 
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 
 class Snake:
 
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -21,4 +27,21 @@ class Snake:
             new_x = self.segments[s - 1].xcor()
             new_y = self.segments[s - 1].ycor()
             self.segments[s].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.head.forward(MOVE_DISTANCE)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.segments[0].setheading(RIGHT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
