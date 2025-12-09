e2b3de811682bedc894d3ab2e33b02920d2fdc82
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-09 08:43:05 +0200

    screen

--- Day022/main.py
+++ Day022/main.py
@@ -3,7 +3,7 @@ from turtle import Screen
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
-screen.title("Paddle Game")
+screen.title("Pong Game")
 
 
 screen.exitonclick()
