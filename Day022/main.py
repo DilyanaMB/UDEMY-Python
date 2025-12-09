835b8f9ee180aa8903f1fa66cbb4b837cffaeecd
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-09 09:16:16 +0200

    create paddles and ball

--- Day022/main.py
+++ Day022/main.py
@@ -1,36 +1,29 @@
 from turtle import Screen, Turtle
 import time
+from ball import Ball
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title("Pong Game")
 screen.tracer(0)
+ball = Ball()
 
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+r_paddle = Paddle(350,0)
+l_paddle = Paddle(-350,0)
 
 screen.listen()
-screen.onkey(go_up,'Up')
-screen.onkey(go_down,'Down')
+screen.onkey(r_paddle.go_up,'Up')
+screen.onkey(r_paddle.go_down,'Down')
+
+screen.onkey(l_paddle.go_up,'w')
+screen.onkey(l_paddle.go_down,'s')
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+    ball.move()
 
 screen.exitonclick()
--- Day022/paddle.py
+++ Day022/paddle.py
@@ -1,11 +1,19 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self,position_x,position_y):
         super().__init__()
         self.shape("square")
         self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
-        self.segments = []
+        self.goto(position_x, position_y)
 
-        self.head = self.segments[0]
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
