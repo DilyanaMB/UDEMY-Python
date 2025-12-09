d28a932eeb951bf4405973a8164808326871c174
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-09 08:55:59 +0200

    create paddle

--- Day022/paddle.py
+++ Day022/paddle.py
@@ -4,3 +4,8 @@ class Paddle(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("square")
+        self.color("white")
+        self.penup()
+        self.segments = []
+
+        self.head = self.segments[0]
