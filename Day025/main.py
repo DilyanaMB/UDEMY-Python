913334bd7a1fc9ad8aaae1a138b32dcb02e3f2d2
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-14 23:47:43 +0200

    average with pandas

--- Day025/main.py
+++ Day025/main.py
@@ -15,3 +15,14 @@
 import pandas
 data =pandas.read_csv('weather_data.csv')
 print(data['temp'])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list =data['temp'].tolist()
+print(temp_list)
+
+average =sum(temp_list) / float(len(temp_list))
+print(average)
+
+print(data['temp'].mean()) # do the same as sum()/len() but with pandas
