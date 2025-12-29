9a23db480f313a57920d460c754a17ef1c7e322b
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-29 12:22:51 +0200

    -

---
+++ Day030/main.py
@@ -0,0 +1,15 @@
+# FileNotFound
+# with open('a_file.text') as f:
+#     f.read()
+
+# KeyError
+# a_dict = {'key': 'value'}
+# value=a_dict['non_existent_key']
+
+# IndexError
+# fruit_list = ['banana', 'apple', 'mango']
+# fruit = fruit_list[3]
+
+#TypeError
+# text ='abc'
+# print(text + 5)
